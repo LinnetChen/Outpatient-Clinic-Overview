bed2e48423d1489ebc967b5ecb032648052e880e
Linnet <linnet_chen@digeam.com>
2024-07-02 20:24:00 +0800

    v1.0.15

--- js/sharedHead.js
+++ js/sharedHead.js
@@ -3,16 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
     <meta charset="UTF-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>院區門診總表</title>
+    <title>院區門診醫師門診表資料</title>
 
-    <meta name="description" content="院區門診總表">
-    <meta property="og:title" content="院區門診總表">
-    <meta property="og:description" content="院區門診總表">
+    <meta name="description" content="院區門診醫師門診表資料">
+    <meta property="og:title" content="院區門診醫師門診表資料">
+    <meta property="og:description" content="院區門診醫師門診表資料">
     <meta property="og:type" content="website">
     <meta property="og:image" content="">
 
     
     <link rel="stylesheet" type="text/css" href="https://linnetchen.github.io/Outpatient-Clinic-Overview/css/style.css"></link>
+    <link rel="stylesheet" type="text/css" href="/css/page.css"></link>
     
     <link rel="icon" href="https://docs.dianthus.com.tw/wp-content/uploads/icon-150x150.png" sizes="32x32">
     <link rel="icon" href="https://docs.dianthus.com.tw/wp-content/uploads/icon.png" sizes="192x192">
